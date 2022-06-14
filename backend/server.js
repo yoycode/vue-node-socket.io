35cf35e755fa33abd79cbd198d93f5659b0c9789
nct_jiyeon <ljy7828@gmail.com>
2022-06-14 16:48:05 +0900

    전체 수정

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server,{
+    pingTimeout: 1000,
+    path: '/socket.io',
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
 
-//setting cors 
+
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
 
+// localhost:3000서버에 접속하면 클라이언트로 메세지을 전송한다
 app.get('/', function(req, res) {
     res.sendFile('Hellow Chating App Server');
 });
@@ -16,6 +24,10 @@ app.get('/', function(req, res) {
 //connection event handler
 io.on('connection' , function(socket) {
     console.log('Connect from Client: '+socket)
+
+    socket.on('hello', function(data){
+        console.log('hello from Client: '+data)
+    });
 
     socket.on('chat', function(data){
         console.log('message from Client: '+data.message)
@@ -31,6 +43,6 @@ io.on('connection' , function(socket) {
 
 })
 
-server.listen(3001, function() {
-    console.log('socket io server listening on port 3001')
+server.listen(3000, function() {
+    console.log('socket io server listening on port 3000')
 })
